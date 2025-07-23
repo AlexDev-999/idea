e874ae103a559050ff73a7c0e5862a7b38be0906
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-07-24 00:46:46 +0300

    start white cement section

---
+++ src/data/products/whiteCementData.js
@@ -0,0 +1,67 @@
+export const whiteCementData = [
+  {
+    image: '/webp/whiteCement/portlandСement.webp',
+    titleUk: 'CEM I 52.5 N Портландцемент',
+    titleEn: 'CEM I 52.5 N Portland Cement',
+    clinker: '95 - 100 %',
+    additives: '0 - 5 %',
+    descriptionUk:
+      'Білий портландцемент, виготовлений з чистого клінкеру, ідеально підходить для конструкцій, верхніх покриттів та бетонних швів на видимих поверхнях. Цей продукт дозволяє реалізувати свої творчі здібності до форм, форм та кольорів, забезпечуючи при цьому виняткову міцність. Він також служить чудовою основою для високоефективних бетонів.',
+    descriptionEn:
+      'White Portland cement, made from pure clinker, is ideal for structures, topcoats and concrete joints on visible surfaces. This product allows you to realize your creative abilities in shapes, forms and colors, while providing exceptional strength. It also serves as an excellent base for high-performance concretes.',
+    deliveryOptionsUk: [
+      'Насипом в автоцистерні.',
+      'В мішках по 25кг (48 мішків на палеті).',
+      'В біг-бегах по 1т.',
+    ],
+    deliveryOptionsEn: [
+      'In bulk in a tanker truck.',
+      'In 25kg bags (48 bags on a pallet).',
+      'In big-bags for 1t.',
+    ],
+  },
+  {
+    image: '/webp/whiteCement/portlandSlag.webp',
+    titleUk: 'CEM II/A-S 52.5 N Портландський шлаковий',
+    titleEn: 'CEM II/A-S 52.5 N Portland slag',
+    clinker: '80 - 94 %',
+    blastFurnaceSlag: '6 - 20 %',
+    additives: '0 - 5 %',
+    descriptionUk:
+      'Білий портландцемент пропонує поєднання високої початкової міцності та постійного нарощування міцності завдяки додаванню шлаку. Його внутрішня структура підвищує довговічність, що робить його чудовим вибором для оздоблення цивільних конструкцій, виготовлених з високоміцного бетону, навіть у кольоровому виконанні.',
+    descriptionEn:
+      'White Portland cement offers a combination of high initial strength and continuous strength development through the addition of slag. Its internal structure increases durability, making it an excellent choice for finishing civil structures made of high-strength concrete, even in colored versions.',
+    deliveryOptionsUk: [
+      'Насипом в автоцистерні.',
+      'В мішках по 25кг (48 мішків на палеті).',
+      'В біг-бегах по 1т.',
+    ],
+    deliveryOptionsEn: [
+      'In bulk in a tanker truck.',
+      'In 25kg bags (48 bags on a pallet).',
+      'In big-bags for 1t.',
+    ],
+  },
+  {
+    image: '/webp/whiteCement/portlandLimestoneCement.webp',
+    titleUk: 'CEM II/A-LL 42.5 R Портлендський вапняковий цемент',
+    titleEn: 'CEM II/A-LL 42.5 R Portland Limestone Cement',
+    clinker: '80 - 94 %',
+    limestone: '6 - 20 %',
+    additives: '0 - 5 %',
+    descriptionUk:
+      'Білий портландцемент з вапняком добре підходить для конструкцій та самоущільнювального бетону на видимих поверхнях. Він створює гладкі, індивідуальні поверхні, що дозволяють створювати різноманітні форми та кольори. Цей продукт також забезпечує надійну структурну міцність.',
+    descriptionEn:
+      'White Portland Cement with Limestone is well suited for structures and self-compacting concrete on exposed surfaces. It creates smooth, individual surfaces that allow for a variety of shapes and colors. This product also provides reliable structural strength.',
+    deliveryOptionsUk: [
+      'Насипом в автоцистерні.',
+      'В мішках по 25кг (48 мішків на палеті).',
+      'В біг-бегах по 1т.',
+    ],
+    deliveryOptionsEn: [
+      'In bulk in a tanker truck.',
+      'In 25kg bags (48 bags on a pallet).',
+      'In big-bags for 1t.',
+    ],
+  },
+];
--- src/sections/whiteCementSection/WhiteCementSection.jsx
+++ src/sections/whiteCementSection/WhiteCementSection.jsx
@@ -1,7 +1,29 @@
-import styles from "./WhiteCementSection.module.scss";
+import BackHomeProductsBtn from '@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn';
+import { whiteCementData } from '@/data/products/whiteCementData';
+import Image from 'next/image';
+import styles from './WhiteCementSection.module.scss';
 
-const WhiteCementSection = ({ lang, dictionary }) => {
-  return <section className={styles.section}>WhiteCementSection</section>;
+const WhiteCementSection = ({ lang, dictionary, buttons }) => {
+  return (
+    <section className={styles.section}>
+      <div className={`${styles.container} container`}>
+        <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
+
+        <h1 className={styles.title}>{dictionary.title}</h1>
+        <h3 className={styles.subtitle}>{dictionary.description}</h3>
+
+        <ul className={styles.list}>
+          {whiteCementData.map((item, index) => (
+            <li key={index}>
+              <figure className={styles.imgContainer}>
+                <Image src={item.image} fill={true} alt={item.titleUk} />
+              </figure>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default WhiteCementSection;
