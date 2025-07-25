fe45f0bbb6932fd6e72321431384eb47010496e8
Maksimchik36 <Peltek1985@gmail.com>
2025-07-25 21:19:20 +0300

    created MarbleCrumbsPage

--- src/sections/marbleCrumbSection/MarbleCrumbSection.jsx
+++ src/sections/marbleCrumbSection/MarbleCrumbSection.jsx
@@ -1,7 +1,67 @@
+import Image from "next/image";
+import BackHomeProductsBtn from "@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn";
+import { i18n } from "@/dictionaries/i18n.config";
+import { marbleCrumbData } from "@/data/products/marbleCrumbData";
 import styles from "./MarbleCrumbSection.module.scss";
 
 const MarbleCrumbSection = ({ lang, dictionary, buttons }) => {
-  return <section className={styles.section}>MarbleCrumbSection</section>;
+  const factionsList =
+    lang === i18n.locales[0]
+      ? marbleCrumbData.factionsListUk
+      : marbleCrumbData.factionsListEn;
+
+  return (
+    <section className={`section ${styles.section}`}>
+      <div className="container">
+        <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
+        <h1 className={styles.title}>{dictionary.title}</h1>
+        <div className={styles.contentWrapper}>
+          <div className={styles.imgWrapper}>
+            <Image
+              className={styles.img}
+              src={marbleCrumbData.img}
+              alt={
+                lang === i18n.locales[0]
+                  ? marbleCrumbData.titleUk
+                  : marbleCrumbData.titleEn
+              }
+              // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+              sizes="50vw"
+              fill={true}
+              priority={true}
+            />
+          </div>
+          <div className={styles.infoWrapper}>
+            <h2 className={styles.productTitle}>
+              {lang === i18n.locales[0]
+                ? marbleCrumbData.titleUk
+                : marbleCrumbData.titleEn}
+            </h2>
+            <p className={styles.productDescription}>
+              {lang === i18n.locales[0]
+                ? marbleCrumbData.descriptionUk_1
+                : marbleCrumbData.descriptionEn_1}
+            </p>
+            <p
+              className={`${styles.productDescription} ${styles.lastDescription}`}
+            >
+              {lang === i18n.locales[0]
+                ? marbleCrumbData.descriptionUk_2
+                : marbleCrumbData.descriptionEn_2}
+            </p>
+            <p className={styles.factionsSubTitle}>{dictionary.subTitle}</p>
+            <ul className={styles.factionsList}>
+              {factionsList.map((item) => (
+                <li key={item} className={styles.factionItem}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MarbleCrumbSection;
