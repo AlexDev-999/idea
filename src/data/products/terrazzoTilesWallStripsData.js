a7af803281217551582e1ce98f3d259fc7fbd756
Maksimchik36 <Peltek1985@gmail.com>
2025-07-26 13:17:36 +0300

    created component TerrazzoTilesWallStrips for TerrazzoTilesIdSection

---
+++ src/data/products/terrazzoTilesWallStripsData.js
@@ -0,0 +1,6 @@
+export const terrazzoTilesWallStripsData = {
+    mainImg:"/webp/terrazzoTilesWallStripsPage/wall-strips-main.webp",
+    altMainImg:"Wall strips",
+    highStripsImgs:[{img:"/webp/terrazzoTilesWallStripsPage/high-strips-1.webp", altImg:"High wall strips 1"}, {img:"/webp/terrazzoTilesWallStripsPage/high-strips-2.webp", altImg:"High wall strips 2"}],
+    roundedStripsImgs:[{img:"/webp/terrazzoTilesWallStripsPage/rounded-strips-1.webp", altImg:"Rounded wall strips 1"},{img:"/webp/terrazzoTilesWallStripsPage/rounded-strips-2.webp", altImg:"Rounded wall strips 2"}]
+}
