7373e94c6293408af684646f77e5e22dcad8ba74
Maksimchik36 <Peltek1985@gmail.com>
2025-07-31 14:00:32 +0300

    created adaptive for limeSection

--- src/sections/limeSection/LimeSection.jsx
+++ src/sections/limeSection/LimeSection.jsx
@@ -3,7 +3,6 @@ import BackHomeProductsBtn from "@/components/buttons/BackHomeProductsBtn/BackHo
 import OpenPdfBtn from "@/components/buttons/OpenPdfBtn/OpenPdfBtn";
 import QuickLimeTable from "@/components/tables/QuickLimeTable/QuickLimeTable";
 import SlakedLimeTable from "@/components/tables/SlakedLimeTable/SlakedLimeTable";
-import { i18n } from "@/dictionaries/i18n.config";
 import styles from "./LimeSection.module.scss";
 
 const LimeSection = ({ lang, dictionary, buttons }) => {
@@ -14,63 +13,62 @@ const LimeSection = ({ lang, dictionary, buttons }) => {
         <h1 className={styles.title}>{dictionary.title}</h1>
         <p className={styles.description}>{dictionary.description}</p>
         <ul>
-          {dictionary.productsData.map((item, index) => 
-              <li key={item.slug} className={styles.productItem}>
-                <div className={styles.contentWrapper}>
-                  <div className={styles.imgWrapper}>
-                    <Image
-                      className={styles.img}
-                      src={item.img}
-                      alt={item.title}
-                      // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
-                      sizes="50vw"
-                      fill={true}
-                      priority={index === 0 ? true : false}
-                    />
-                  </div>
-                  <div className={styles.infoWrapper}>
-                    <h2 className={styles.productTitle}>
-                      {item.title}
-                    </h2>
-                    <p className={styles.productDescription}>
-                      {item.description}
-                    </p>
-                    <p className={styles.applicationSubTitle}>
-                      {dictionary.application}
-                    </p>
-                    <ul className={styles.applicationList}>
-                      {item.applicationList.map((el, index) => (
-                        <li key={index} className={styles.applicationItem}>
-                          {el}
-                        </li>
-                      ))}
-                    </ul>
+          {dictionary.productsData.map((item, index) => (
+            <li key={item.slug} className={styles.productItem}>
+              <div className={styles.contentWrapper}>
+                <div className={styles.imgWrapper}>
+                  <Image
+                    className={styles.img}
+                    src={item.img}
+                    alt={item.title}
+                    // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+                    sizes="50vw"
+                    fill={true}
+                    priority={index === 0 ? true : false}
+                  />
+                </div>
+                <div className={styles.infoWrapper}>
+                  <h2 className={styles.productTitle}>{item.title}</h2>
+                  <p className={styles.productDescription}>
+                    {item.description}
+                  </p>
+                  <p className={styles.applicationSubTitle}>
+                    {dictionary.application}
+                  </p>
+                  <ul className={styles.applicationList}>
+                    {item.applicationList.map((el, index) => (
+                      <li key={index} className={styles.applicationItem}>
+                        {el}
+                      </li>
+                    ))}
+                  </ul>
 
-                    <p className={styles.deliverySubTitle}>
-                      {dictionary.delivery}
-                    </p>
-                    <ul className={styles.deliveryList}>
-                      {item.deliveryList.map((el, index) => (
-                        <li key={index} className={styles.deliveryItem}>
-                          {el}
-                        </li>
-                      ))}
-                    </ul>
-                    <div className={styles.techInfoWrapper}>
-                      <span className={styles.techInfoSubTitle}>
-                        {dictionary.techInfo}
-                      </span>
-                      <OpenPdfBtn
-                        href={item.pdfPath}
-                        text={buttons.openPdfBtn}
-                      />
-                    </div>
+                  <p className={styles.deliverySubTitle}>
+                    {dictionary.delivery}
+                  </p>
+                  <ul className={styles.deliveryList}>
+                    {item.deliveryList.map((el, index) => (
+                      <li key={index} className={styles.deliveryItem}>
+                        {el}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className={styles.techInfoWrapper}>
+                    <span className={styles.techInfoSubTitle}>
+                      {dictionary.techInfo}
+                    </span>
+                    <OpenPdfBtn href={item.pdfPath} text={buttons.openPdfBtn} />
                   </div>
                 </div>
-                {item.slug === "quicklime" && <QuickLimeTable lang={lang} tableData={item.tableData} />}
-                {item.slug === "slaked-lime" && <SlakedLimeTable lang={lang} tableData={item.tableData}/>}
-              </li>
-           )}
+              </div>
+              {item.slug === "quicklime" && (
+                <QuickLimeTable lang={lang} tableData={item.tableData} />
+              )}
+              {item.slug === "slaked-lime" && (
+                <SlakedLimeTable lang={lang} tableData={item.tableData} />
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </section>
