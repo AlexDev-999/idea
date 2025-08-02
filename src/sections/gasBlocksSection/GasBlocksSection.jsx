051830bc63ad45c03b55c876f62169cba71ce9db
Maksimchik36 <Peltek1985@gmail.com>
2025-08-02 20:12:02 +0300

    create adaptive for gasBlocksPage

--- src/sections/gasBlocksSection/GasBlocksSection.jsx
+++ src/sections/gasBlocksSection/GasBlocksSection.jsx
@@ -12,36 +12,44 @@ const GasBlocksSection = ({ lang, dictionary, buttons }) => {
         <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
         <h1 className={styles.title}>{dictionary.title}</h1>
         <p className={styles.description}>{dictionary.description_1}</p>
-        <p className={styles.description}>{dictionary.description_2}</p>
+        <p className={`${styles.description} ${styles.lastDescription}`}>
+          {dictionary.description_2}
+        </p>
         <ul className={styles.productsList}>
           {dictionary.productsData.map((item, index) => (
             <li key={item.slug} className={styles.productItem}>
-              <div className={styles.imgWrapper}>
-                <Image
-                  className={styles.img}
-                  src={item.img}
-                  alt={item.title}
-                  // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
-                  sizes="50vw"
-                  fill={true}
-                  priority={index === 0 ? true : false}
-                />
+              <div className={styles.contentWrapper}>
+                <div className={styles.imgWrapper}>
+                  <Image
+                    className={styles.img}
+                    src={item.img}
+                    alt={item.title}
+                    // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+                    sizes="50vw"
+                    fill={true}
+                    priority={index === 0 ? true : false}
+                  />
+                </div>
+                <div className={styles.infoWrapper}>
+                  <h2 className={styles.productTitle}>{item.title}</h2>
+                  <p className={styles.productDescription}>
+                    {item.description}
+                  </p>
+                </div>
               </div>
-              <div className={styles.infoWrapper}>
-                <h2 className={styles.productTitle}>
-                  {item.title}
-                </h2>
-                <p className={styles.productDescription}>
-                  {item.description}
-                </p>
-
-                {item.slug === "ytong" && <GasBlocksYtongTable lang={lang} tableData={item.tableData}/>}
-              </div>
+              {item.slug === "ytong" && (
+                <GasBlocksYtongTable lang={lang} tableData={item.tableData} />
+              )}
             </li>
           ))}
         </ul>
-        <p className={styles.anotherProductsSubTitle}>{dictionary.anotherProductsSubTitle}</p>
-        <GasBlocksGazobetTable lang={lang} tableData={dictionary.gazobetTableData}/>
+        <p className={styles.anotherProductsSubTitle}>
+          {dictionary.anotherProductsSubTitle}
+        </p>
+        <GasBlocksGazobetTable
+          lang={lang}
+          tableData={dictionary.gazobetTableData}
+        />
       </div>
     </section>
   );
