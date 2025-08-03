6c559dbcf7d52f11c367f4f1f1014d602d9f815f
Maksimchik36 <Peltek1985@gmail.com>
2025-08-03 15:52:40 +0300

    created adaptive for marbleCrumbsPage and flooringStripsPage

--- src/components/TerrazzoTilesFlooringStrips/TerrazzoTilesFlooringStrips.jsx
+++ src/components/TerrazzoTilesFlooringStrips/TerrazzoTilesFlooringStrips.jsx
@@ -1,56 +1,60 @@
 import Image from "next/image";
-import BackHomeProductsBtn from "@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn";
-import styles from './TerrazzoTilesFlooringStrips.module.scss';
+// import BackHomeProductsBtn from "@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn";
+import BreadCrumbs from "../buttons/BreadCrumbs/BreadCrumbs";
+import styles from "./TerrazzoTilesFlooringStrips.module.scss";
 
-const TerrazzoTilesFlooringStrips = ({lang, dictionary, buttons}) => {
+const TerrazzoTilesFlooringStrips = ({ lang, dictionary, buttons }) => {
   return (
     <div className="container">
-        <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
-        <h1 className={styles.title}>{dictionary.idTitle}</h1>
-        <div className={styles.contentWrapper}>
-          <div className={styles.imgWrapper}>
-            <Image
-              className={styles.img}
-              src={dictionary.idMainImg}
-              alt={dictionary.idTitle}
-              // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
-              sizes="50vw"
-              fill={true}
-              priority={true}
-            />
-          </div>
-          <div className={styles.infoWrapper}>          
-                
-            <ul className={styles.sizesWrapper}>{dictionary.idSizes.map((item, index)=>
+      {/* <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} /> */}
+      <BreadCrumbs
+        href="/plitka-teraczo"
+        backTitle={dictionary.navParrentPage}
+        nowtitle={dictionary.navCurrentPage}
+      />
+      <h1 className={styles.title}>{dictionary.idTitle}</h1>
+      <div className={styles.contentWrapper}>
+        <div className={styles.imgWrapper}>
+          <Image
+            className={styles.img}
+            src={dictionary.idMainImg}
+            alt={dictionary.idTitle}
+            // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+            sizes="50vw"
+            fill={true}
+            priority={true}
+          />
+        </div>
+        <div className={styles.infoWrapper}>
+          <ul className={styles.sizesWrapper}>
+            {dictionary.idSizes.map((item, index) => (
               <li key={index} className={styles.sizesItem}>
                 <p className={styles.sizesText}>{item.title.toUpperCase()}</p>
                 <p className={styles.sizesValue}>{item.value}</p>
-                </li>)}                
-            </ul>  
-            <p className={styles.description}>
-              {dictionary.idText}
-            </p>            
-          </div>
+              </li>
+            ))}
+          </ul>
+          <p>{dictionary.idText}</p>
         </div>
-    
-        <ul className={styles.roundedStripsList}>
-            {dictionary.idImages.map((item) => (
-                <li key={item.altImg} className={styles.roundedStripsItem}>
-                     <Image
-              className={styles.roundedStripImg}
+      </div>
+
+      <ul className={styles.anotherStripsList}>
+        {dictionary.idImages.map((item) => (
+          <li key={item.altImg} className={styles.anotherStripsItem}>
+            <Image
+              className={styles.anotherStripImg}
               src={item.path}
-              alt={item.altImg               
-              }
+              alt={item.altImg}
               // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
               sizes="25vw"
               fill={true}
               priority={true}
             />
-                </li>
-              ))}
-        </ul>
-      </div>
-      )
-}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TerrazzoTilesFlooringStrips
+export default TerrazzoTilesFlooringStrips;
--- src/sections/terrazzoTilesIdSection/TerrazzoTilesIdSection.jsx
+++ src/sections/terrazzoTilesIdSection/TerrazzoTilesIdSection.jsx
@@ -1,17 +1,16 @@
-import TerrazzoTilesClassic from '@/components/TerrazzoTilesClassic/TerrazzoTilesClassic';
-import TerrazzoTilesFlooringStrips from '@/components/TerrazzoTilesFlooringStrips/TerrazzoTilesFlooringStrips';
-import TerrazzoTilesBeatrix from '@/components/TerrazzoTilesBeatrix/TerrazzoTilesBeatrix';
-import TerrazzoTilesWallStrips from '@/components/TerrazzoTilesWallStrips/TerrazzoTilesWallStrips';
-import styles from './TerrazzoTilesIdSection.module.scss';
+import TerrazzoTilesClassic from "@/components/TerrazzoTilesClassic/TerrazzoTilesClassic";
+import TerrazzoTilesFlooringStrips from "@/components/TerrazzoTilesFlooringStrips/TerrazzoTilesFlooringStrips";
+import TerrazzoTilesBeatrix from "@/components/TerrazzoTilesBeatrix/TerrazzoTilesBeatrix";
+import TerrazzoTilesWallStrips from "@/components/TerrazzoTilesWallStrips/TerrazzoTilesWallStrips";
+import styles from "./TerrazzoTilesIdSection.module.scss";
 
 const TerrazzoTilesIdSection = ({ lang, dictionary, buttons }) => {
-  console.log('dictionary', dictionary);
-  const tilesClassicSlugs = ['tereza', 'margita', 'radka', 'xena', 'ornela'];
+  // console.log('dictionary', dictionary);
+  const tilesClassicSlugs = ["tereza", "margita", "radka", "xena", "ornela"];
   const beatrixSlugs = ["beatrix", "beatrix-large"];
 
   return (
     <section className={`section ${styles.section}`}>
-
       {tilesClassicSlugs.includes(dictionary.slug) && (
         <TerrazzoTilesClassic
           lang={lang}
@@ -20,21 +19,29 @@ const TerrazzoTilesIdSection = ({ lang, dictionary, buttons }) => {
         />
       )}
 
-      {dictionary.slug === "flooring-strips" && <TerrazzoTilesFlooringStrips lang={lang}
+      {dictionary.slug === "flooring-strips" && (
+        <TerrazzoTilesFlooringStrips
+          lang={lang}
           dictionary={dictionary}
-          buttons={buttons}/>}
-      
-      {beatrixSlugs.includes(dictionary.slug) &&<TerrazzoTilesBeatrix lang={lang}
-          dictionary={dictionary}
-          buttons={buttons}/>}
+          buttons={buttons}
+        />
+      )}
 
-      {dictionary.slug === 'wall-strips' && (
+      {beatrixSlugs.includes(dictionary.slug) && (
+        <TerrazzoTilesBeatrix
+          lang={lang}
+          dictionary={dictionary}
+          buttons={buttons}
+        />
+      )}
+
+      {dictionary.slug === "wall-strips" && (
         <TerrazzoTilesWallStrips
           lang={lang}
           dictionary={dictionary}
           buttons={buttons}
         />
-      )}    
+      )}
     </section>
   );
 };
