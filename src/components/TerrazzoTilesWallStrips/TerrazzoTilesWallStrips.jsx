311ee9891e55ecc256b2c1573f60aed9595e4417
Maksimchik36 <Peltek1985@gmail.com>
2025-08-03 19:39:37 +0300

    created adaptive for wallStripPage, fixed another styles

--- src/components/TerrazzoTilesWallStrips/TerrazzoTilesWallStrips.jsx
+++ src/components/TerrazzoTilesWallStrips/TerrazzoTilesWallStrips.jsx
@@ -1,72 +1,78 @@
 import Image from "next/image";
-import BackHomeProductsBtn from "@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn";
-import styles from './TerrazzoTilesWallStrips.module.scss';
+import BreadCrumbs from "../buttons/BreadCrumbs/BreadCrumbs";
+import styles from "./TerrazzoTilesWallStrips.module.scss";
 
-const TerrazzoTilesWallStrips = ({lang, dictionary, buttons}) => {
+const TerrazzoTilesWallStrips = ({ dictionary }) => {
   return (
     <div className="container">
-        <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
-        <h1 className={styles.title}>{dictionary.idTitle}</h1>
-        <div className={styles.contentWrapper}>
-          <div className={styles.imgWrapper}>
-            <Image
-              className={styles.img}
-              src={dictionary.idMainImg}
-              alt={dictionary.idTitle}
-              // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
-              sizes="50vw"
-              fill={true}
-              priority={true}
-            />
-          </div>
-          <div className={styles.infoWrapper}>           
-            <p className={styles.description}>
-              {dictionary.idDescription}
-            </p>      
-            <ul className={styles.sizesWrapper}>{dictionary.idSizes.map((item, index)=>
+      <BreadCrumbs
+        href="/plitka-teraczo"
+        backTitle={dictionary.navParrentPage}
+        nowtitle={dictionary.navCurrentPage}
+      />
+      <h1 className={styles.title}>{dictionary.idTitle}</h1>
+      <div className={styles.contentWrapper}>
+        <div className={styles.imgWrapper}>
+          <Image
+            className={styles.img}
+            src={dictionary.idMainImg}
+            alt={dictionary.idTitle}
+            // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+            sizes="50vw"
+            fill={true}
+            priority={true}
+          />
+        </div>
+        <div className={styles.infoWrapper}>
+          <p className={styles.description}>{dictionary.idDescription}</p>
+          <ul className={styles.sizesWrapper}>
+            {dictionary.idSizes.map((item, index) => (
               <li key={index} className={styles.sizesItem}>
                 <p className={styles.sizesText}>{item.title.toUpperCase()}</p>
                 <p className={styles.sizesValue}>{item.value}</p>
-                </li>)}                
-            </ul>    
-            <p className={styles.opportunityText}>{dictionary.idOpportunityText}</p>
-            <ul className={styles.highStripsList}>
-              {dictionary.idHighStripsImgs.map((item) => (
-                <li key={item.altImg} className={styles.highStripsItem}>
-                     <Image
-              className={styles.highStripImg}
-              src={item.img}
-              alt={item.altImg               
-              }
-              // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
-              sizes="25vw"
-              fill={true}
-              priority={true}
-            />
-                </li>
-              ))}
-            </ul>
-          </div>
+              </li>
+            ))}
+          </ul>
+          <p className={styles.opportunityText}>
+            {dictionary.idOpportunityText}
+          </p>
+          <ul className={styles.highStripsList}>
+            {dictionary.idHighStripsImgs.map((item) => (
+              <li key={item.altImg} className={styles.highStripsItem}>
+                <Image
+                  className={styles.highStripImg}
+                  src={item.img}
+                  alt={item.altImg}
+                  // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
+                  sizes="25vw"
+                  fill={true}
+                  priority={true}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-        <p className={styles.opportunitySubTitle}>{dictionary.idOpportunitySubTitle}</p>
-        <ul className={styles.roundedStripsList}>
-            {dictionary.idRoundedStripsImgs.map((item) => (
-                <li key={item.altImg} className={styles.roundedStripsItem}>
-                     <Image
+      </div>
+      <p className={styles.opportunitySubTitle}>
+        {dictionary.idOpportunitySubTitle}
+      </p>
+      <ul className={styles.roundedStripsList}>
+        {dictionary.idRoundedStripsImgs.map((item) => (
+          <li key={item.altImg} className={styles.roundedStripsItem}>
+            <Image
               className={styles.roundedStripImg}
               src={item.img}
-              alt={item.altImg               
-              }
+              alt={item.altImg}
               // sizes="(max-width: 767px) 95vw, (max-width: 1439px) 42vw, 1200px"
               sizes="25vw"
               fill={true}
               priority={true}
             />
-                </li>
-              ))}
-        </ul>
-      </div>
-      )
-}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TerrazzoTilesWallStrips
+export default TerrazzoTilesWallStrips;
