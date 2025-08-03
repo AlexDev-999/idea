a031d1809d7f2fe6a1e8974905bb6146e79f5a00
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-08-03 23:10:53 +0300

    fix breadCrubs, add images list in stairs section and refactoring code

--- src/sections/stairsSection/StairsSection.jsx
+++ src/sections/stairsSection/StairsSection.jsx
@@ -2,20 +2,21 @@ import Image from 'next/image';
 import { i18n } from '@/dictionaries/i18n.config';
 import BackHomeProductsBtn from '@/components/buttons/BackHomeProductsBtn/BackHomeProductsBtn';
 import styles from './StairsSection.module.scss';
+import ProductsIdList from '@/components/ProductsIdList/ProductsIdList';
 
 const StairsSection = ({ lang, dictionary, buttons }) => {
   return (
     <section className={styles.section}>
-      <div className={` container`}>
+      <div className={` container ${styles.container}`}>
         <BackHomeProductsBtn lang={lang} text={buttons.backHomeProductsBtn} />
 
         <h1 className={styles.title}>{dictionary.title}</h1>
         <h3 className={styles.subtitle}>{dictionary.subtitle}</h3>
 
-        <div className={styles.container}>
+        <div className={styles.containerContent}>
           <figure className={styles.imgContainer}>
             <Image
-              src={dictionary.image}
+              src={dictionary.images[0]}
               alt={dictionary.title}
               fill={true}
               sizes="(max-width: 767px) 320px, (max-width: 1439px) 352px, 1200px"
@@ -32,6 +33,7 @@ const StairsSection = ({ lang, dictionary, buttons }) => {
             <li className={styles.desc}>{dictionary.description_4}</li>
           </ul>
         </div>
+        <ProductsIdList data={dictionary} />
       </div>
     </section>
   );
