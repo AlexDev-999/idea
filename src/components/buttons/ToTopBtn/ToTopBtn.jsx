ccc1e4ffb8eae92f2f4ac8d31e192eda486cdb9f
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-08-06 21:17:01 +0300

    add toTopBtn

---
+++ src/components/buttons/ToTopBtn/ToTopBtn.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { scrollToTop } from '@/helpers/scrollToTop';
+
+import styles from './ToTopBtn.module.scss';
+
+const ToTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={isVisible ? styles.toTopBtn : styles.toTopBtnHidden}
+      title="Кнопка до верху сторінки"
+      aria-label="Кнопка до гори"
+    >
+      <svg className={styles.svg}>
+        <use href="sprite.svg#icon-schevron_right" />
+      </svg>
+    </button>
+  );
+};
+
+export default ToTopBtn;
