fa0abc606cd089cd2e1d5303dc2b5f2e149d5196
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-08-09 15:24:48 +0300

    add link productsList

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,20 +1,25 @@
-import Image from "next/image";
-import CustomLink from "../CustomLink/CustomLink";
-import styles from "./ProductsList.module.scss";
+import Image from 'next/image';
+import Link from 'next/link';
+import { i18n } from '@/dictionaries/i18n.config';
+import CustomLink from '../CustomLink/CustomLink';
+import styles from './ProductsList.module.scss';
 
 const ProductsList = ({ dictionary, href, lang, buttons }) => {
+  const isDefaultLang = lang === i18n.defaultLocale;
+  const path = isDefaultLang ? href : `/${lang}/${href}`;
+
   return (
     <ul className={styles.list}>
       {dictionary.productsData.map((item, index) => (
         <li key={index} className={styles.item}>
-          <figure className={styles.imgContainer}>
+          <Link className={styles.imgContainer} href={`${path}/${item.slug}`}>
             <Image
-              src={href === "dekorativnij-kamin" ? item.images[0] : item.image}
+              src={href === 'dekorativnij-kamin' ? item.images[0] : item.image}
               alt={item.title}
               fill={true}
               sizes="(max-width: 767px) 318px, (max-width: 1439px) 222px, 1200px"
             />
-          </figure>
+          </Link>
           <div className={styles.content}>
             <h3 className={styles.subtitle}>{item.title}</h3>
             <CustomLink
