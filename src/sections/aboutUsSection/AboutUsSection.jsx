823314dddbe12dd1daba5bb4272ab030d983e97d
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-08-15 23:00:38 +0300

    add AboutUs section and fix stairsSection

--- src/sections/aboutUsSection/AboutUsSection.jsx
+++ src/sections/aboutUsSection/AboutUsSection.jsx
@@ -1,7 +1,44 @@
-import styles from "./AboutUsSection.module.scss";
+'use client';
+
+import { useWindowResize } from '@/hooks/windowResize';
+import Image from 'next/image';
+import styles from './AboutUsSection.module.scss';
 
 const AboutUsSection = ({ lang, dictionary }) => {
-  return <section className={styles.section}>AboutUsSection</section>;
+  const { isDesktop } = useWindowResize();
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        {isDesktop && <h1 className={styles.title}>{dictionary.title}</h1>}
+        <div className={styles.contentContainer}>
+          <div>
+            {!isDesktop && <h1 className={styles.title}>{dictionary.title}</h1>}
+            <h3 className={styles.subtitle}>{dictionary.subtitle}</h3>
+          </div>
+          <figure className={styles.imgContainer}>
+            <Image
+              src={dictionary.images[0]}
+              alt={dictionary.title}
+              fill={true}
+              sizes="(max-width: 767px) 320px, (max-width: 1439px) 352px, 1200px"
+            />
+          </figure>
+        </div>
+        <ul className={styles.imgList}>
+          {dictionary.images.slice(1).map((img, index) => (
+            <li key={index} className={styles.imgItem}>
+              <Image
+                src={img}
+                alt={`${dictionary.title} ${index}`}
+                fill={true}
+                sizes="(max-width: 767px) 320px, (max-width: 1439px) 337px, 1200px"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default AboutUsSection;
